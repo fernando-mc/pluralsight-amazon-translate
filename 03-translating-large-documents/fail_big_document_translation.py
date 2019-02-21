5ba1a202851a8a58c82997fe2d1d8c15583b2bb5
Fernando Medina Corey <fmcorey@gmail.com>
2019-02-21 00:25:19 -0800

    Linting errors

--- 03-translating-large-documents/fail_big_document_translation.py
+++ 03-translating-large-documents/fail_big_document_translation.py
@@ -1,8 +1,9 @@
 import boto3
 
-# INPUT_DOCUMENT_NAME = 'small_document.txt'
+INPUT_DOCUMENT_NAME = 'small_document.txt'
 # INPUT_DOCUMENT_NAME = 'big_document.txt'
 OUTPUT_DOCUMENT_NAME = 'document_es.txt'
+
 
 def translate_text(text, lang_code):
     translate = boto3.client('translate')
@@ -13,12 +14,14 @@ def translate_text(text, lang_code):
     )
     print(
         'Translated text into the language code of ' + lang_code + '.' +
-        '\nSource text: "' + text + '"' + 
+        '\nSource text: "' + text + '"' +
         '\nResult text: "' + result['TranslatedText'] + '"\n'
         )
     return result['TranslatedText']
 
-with open(INPUT_DOCUMENT_NAME, 'r') as infile, open(OUTPUT_DOCUMENT_NAME, 'w') as outfile:
+
+with open(INPUT_DOCUMENT_NAME, 'r') as infile, \
+     open(OUTPUT_DOCUMENT_NAME, 'w') as outfile:
     text = infile.read()
     result = translate_text(text, 'es')
     outfile.write(result)
--- 03-translating-large-documents/translate_big_document.py
+++ 03-translating-large-documents/translate_big_document.py
@@ -2,6 +2,7 @@ import boto3
 
 INPUT_DOCUMENT_NAME = 'big_document.txt'
 
+
 def translate_text(text, lang_code):
     translate = boto3.client('translate')
     result = translate.translate_text(
@@ -11,16 +12,18 @@ def translate_text(text, lang_code):
     )
     print(
         'Translated text into the language code of ' + lang_code + '.' +
-        '\nSource text: "' + text + '"' + 
+        '\nSource text: "' + text + '"' +
         '\nResult text: "' + result['TranslatedText'] + '"\n'
         )
     return result['TranslatedText']
 
+
 def translate_large_document(input_filename, output_filename, lang_code):
-    # Placeholder for parts of translated document 
+    # Placeholder for parts of translated document
     final_document_array = []
     # Read the initial document text
-    with open(input_filename, 'r') as infile, open(output_filename, 'w') as outfile:
+    with open(input_filename, 'r') as infile, \
+         open(output_filename, 'w') as outfile:
         text = infile.read()
         # Split it up by paragraph and clean up unused spaces
         text_array = text.split('\n')
@@ -32,5 +35,6 @@ def translate_large_document(input_filename, output_filename, lang_code):
         # Write the translated paragraphs to a new file
         for paragraph in final_document_array:
             outfile.write(paragraph)
+
 
 translate_large_document(INPUT_DOCUMENT_NAME, 'big_document_es.txt', 'es')
