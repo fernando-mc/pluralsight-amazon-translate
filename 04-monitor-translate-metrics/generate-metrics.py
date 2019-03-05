853a639be0df6c04cdb8f5d739d85aaf50498166
Fernando Medina Corey <fmcorey@gmail.com>
2019-03-05 15:29:24 -0800

    Fix typo

--- 04-monitor-translate-metrics/generate-metrics.py
+++ 04-monitor-translate-metrics/generate-metrics.py
@@ -20,7 +20,7 @@ def translate_text(text, lang_code):
 
 text_segments = [
     "Hello!",
-    "This is Plrualsight!",
+    "This is Pluralsight!",
     "What's your name?",
     "Nice to meet you!",
     "Congratulations!",
